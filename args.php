1c09c372df1521a797b8c07260a8e1abe8518aed
dev <dev@dev5.bioshazard.com>
2014-05-14 20:33:53 -0400

    'fixed'

--- args.php
+++ args.php
@@ -6,10 +6,13 @@
 	
 	if(count($pages))
 	{
-		$args = '<select name="ini" id="">';
+		$args = '<select name="ini" id="">
+			<option value="">-- Select an article --</option>';
 		foreach($pages as $page)
 			$args .= '<option value="'.$page['id'].'">'.$page['id'].' - '.$page['title'].'</option>';
 		$args .= '</select> or ';
+		
+		$args = str_replace('value="'.$save['ini'].'"', 'value="'.$save['ini'].'" selected="selected"', $args);
 	}
 	
 	$args .= '<a href="%baseurl%apps/manage/pages/new/">Create New Page</a>';
