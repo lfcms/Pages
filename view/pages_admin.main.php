b77558a773cb7a7408b997e86d18e93c1a28bccb
dev <dev@wightdev.com>
2015-04-17 00:02:56 -0400

    'admin done'

--- view/pages_admin.main.php
+++ view/pages_admin.main.php
@@ -1,11 +1,11 @@
-<a href="%appurl%newarticle/" class="blue button marbot">Create New Page</a>
+<a href="%appurl%newarticle/" class="blue button marbot"><i class="fa fa-plus"></i> New Page</a>
 
-<ol class="efvlist">
+<ol class="efvlist rounded">
 	<?php foreach($pages as $page): ?>
 	
-	<li>
-		<a <?=jsprompt();?> class="x pull-right" href="%appurl%rm/<?=$page['id'];?>/">x</a> 
+	<li> 
 		<a href="%appurl%edit/<?=$page['id'];?>/"><?=$page['title'];?></a>
+		<a <?=jsprompt();?> class="x pull-right" href="%appurl%rm/<?=$page['id'];?>/"><i class="fa fa-trash"></i></a>
 	</li>
 	
 	<?php endforeach; ?>
