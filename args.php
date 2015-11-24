efe49cf60bdfb8b5338c880effc2f076c9b56182
dev <dev@dev5.bioshazard.com>
2015-11-24 01:31:43 -0500

    'clicks direct to page edit'

--- args.php
+++ args.php
@@ -6,16 +6,23 @@
 	
 	$pages = (new LfPages)->cols('id, title')->getAll();
 	
+	$args = '';
 	if(count($pages))
 	{
-		$args = '<select name="ini" id="">
+		$args .= '<select name="ini">
 			<option value="">-- Select an article --</option>';
 		foreach($pages as $page)
 			$args .= '<option value="'.$page['id'].'">'.$page['id'].' - '.$page['title'].'</option>';
-		$args .= '</select> or ';
+		$args .= '</select> ';
 		
+		$args .= '<a href="%baseurl%apps/pages/edit/'.$save['ini'].'">Edit this Page</a>  or ';
+		
+			
 		$args = str_replace('value="'.$save['ini'].'"', 'value="'.$save['ini'].'" selected="selected"', $args);
 	}
 	
-	$args .= '<a href="%baseurl%apps/manage/pages/new/">Create New Page</a>';
+	$args .= '<a href="%baseurl%apps/pages/new/">Create New Page</a>';
+	
+	/*$args .= '<input type="checkbox" name="ini[title]" checked="checked">
+			Show Title';*/
 ?>
