77858781109822b41b2da618800cc5f11eedbd8c
dev <dev@dev5.bioshazard.com>
2015-11-24 01:42:26 -0500

    'fixed up MVC'

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
-<?php                                                                                                
-                                                                                                     
-include 'model/page.php'; // idk if I need this here...                                              
+<?php                                                                                                       
+				
 include LF.'system/lib/3rdparty/parsedown/Parsedown.php';                                            
-                                                                                                     
-$page = (new Page)                                                                                   
+
+$page = (new LfPages)                                                                                   
         ->byId($_app['ini'])                                                                         
         ->get();                                                                                     
-                                                                                                     
+
 $this->lf->select['title'] = $page['title'];                                                         
-                                                                                                     
+
 $Parsedown = new Parsedown();                                                                        
-echo '<h2>'.$page['title'].'</h2>';                                                                  
-                                                                                                     
+echo '<h2>'.$page['title'].'</h2>';                                                            
+
 // apploader for separate apps.                                                                      
 // this addresses the inability to have a home page composed of                                      
 $frame = $page['content'];                                                                           
-                                                                                                     
+
 $count = 1;                                                                                          
 $cwd = getcwd();                                                                                     
 preg_match_all('/{([^}]+)}/', $frame, $match);                                                       
@@ -23,22 +22,22 @@ foreach($match[1] as $replace)
 {                                                                                                    
         $ini = NULL;                                                                                 
         $vars = '';                                                                                  
-                                                                                                     
+
         if(!preg_match('/^'                                                                          
                 .'(?:([^#?\/]+))'       // [1] app                                                   
                 .'(?:\/([^\/#?]+))'     // [2] /controller                                           
                 .'(?:#([^?]+))?'        // [2] #vars                                                 
                 .'(?:\?(.*))?'          // [3] ?ini                                                  
         .'$/', $replace, $parts)) continue;                                                          
-                                                                                                     
+
         $app = $parts[1];                                                                            
         if(isset($parts[2]) && $parts[2] != '')                                                      
                 $controller = $parts[2];                                                             
         if(isset($parts[3]) && $parts[3] != '')                                                      
                 $vars = explode('/', $parts[3]);                                                     
         if(isset($parts[4]) && $parts[4] != '')                                                      
-                $ini = $parts[4];                                                                    
-                                                                                      
+                $ini = $parts[4];                                                     
+			
         chdir(LF.'apps/'.$app);                                                                      
         $ftimer = microtime(true);                                                                   
         //$frame = str_replace('{'.$replace.'}', $this->apploader($app, $ini, $vars), $frame);       
@@ -52,5 +51,5 @@ er; //timer for app*/
 chdir($cwd);                                                                                         
 
 $frame = (new User)->resolveIds($frame);                                                             
-                                                                                                     
+
 echo $Parsedown->text($frame); 
