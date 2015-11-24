48877c95c3c4a8c3e4b99080aac670f72d39600c
Joe <bioshazard@users.noreply.github.com>
2015-11-24 01:27:32 -0500

    update to app loader
    {app/controller?ini#url/vars}

--- index.php
+++ index.php
@@ -1,46 +1,55 @@
-<?php
-
-include LF.'system/lib/3rdparty/parsedown/Parsedown.php';
-
-$page = (new LfPages)->getById($_app['ini']);
-
-$this->lf->select['title'] = $page['title'];
-
-// apploader for separate apps.
-// this addresses the inability to have a home page composed of 
-$frame = $page['content'];
-
-$count = 1;
-$cwd = getcwd();
-preg_match_all('/{([^}]+)}/', $frame, $match);
-foreach($match[1] as $replace)
-{
-	$ini = NULL;
-	$vars = '';
-	
-	if(!preg_match('/^'
-		.'(?:([^#?]+))' 	// [1] app
-		.'(?:#([^?]+))?' 	// [2] #vars
-		.'(?:\?(.*))?'		// [3] ?ini
-	.'$/', $replace, $parts)) continue;
-	
-	$app = $parts[1];
-	if(isset($parts[2]) && $parts[2] != '') 
-		$vars = explode('/', $parts[2]);
-	if(isset($parts[3]) && $parts[3] != '') 
-		$ini = $parts[3];
-	
-	chdir(LF.'apps/'.$app);
-	$ftimer = microtime(true);
-	//$frame = str_replace('{'.$replace.'}', $this->apploader($app, $ini, $vars), $frame);
-	$frame = str_replace('{'.$replace.'}', $this->apploader($app, $ini, $vars), $frame); // needs to be preg replace so it is replaced 1 time   
-	//$this->lf->timer['Frontpage ('.$count++.') - '.$app.'/'.$this->vars[0]] = microtime(true) - $ftimer; //timer for app
-}
-
-chdir($cwd);
-
-?>
-
-<h2><?=$page['title'];?></h2>
-
-<?=(new Parsedown)->text($frame);?>
+<?php                                                                                                
+                                                                                                     
+include 'model/page.php'; // idk if I need this here...                                              
+include LF.'system/lib/3rdparty/parsedown/Parsedown.php';                                            
+                                                                                                     
+$page = (new Page)                                                                                   
+        ->byId($_app['ini'])                                                                         
+        ->get();                                                                                     
+                                                                                                     
+$this->lf->select['title'] = $page['title'];                                                         
+                                                                                                     
+$Parsedown = new Parsedown();                                                                        
+echo '<h2>'.$page['title'].'</h2>';                                                                  
+                                                                                                     
+// apploader for separate apps.                                                                      
+// this addresses the inability to have a home page composed of                                      
+$frame = $page['content'];                                                                           
+                                                                                                     
+$count = 1;                                                                                          
+$cwd = getcwd();                                                                                     
+preg_match_all('/{([^}]+)}/', $frame, $match);                                                       
+foreach($match[1] as $replace)                                                                       
+{                                                                                                    
+        $ini = NULL;                                                                                 
+        $vars = '';                                                                                  
+                                                                                                     
+        if(!preg_match('/^'                                                                          
+                .'(?:([^#?\/]+))'       // [1] app                                                   
+                .'(?:\/([^\/#?]+))'     // [2] /controller                                           
+                .'(?:#([^?]+))?'        // [2] #vars                                                 
+                .'(?:\?(.*))?'          // [3] ?ini                                                  
+        .'$/', $replace, $parts)) continue;                                                          
+                                                                                                     
+        $app = $parts[1];                                                                            
+        if(isset($parts[2]) && $parts[2] != '')                                                      
+                $controller = $parts[2];                                                             
+        if(isset($parts[3]) && $parts[3] != '')                                                      
+                $vars = explode('/', $parts[3]);                                                     
+        if(isset($parts[4]) && $parts[4] != '')                                                      
+                $ini = $parts[4];                                                                    
+                                                                                                     
+echo LF.'apps/'.$app;                                                                                
+echo '<br>'.$controller;                                                                             
+pre($vars);                                                                                          
+        chdir(LF.'apps/'.$app);                                                                      
+        $ftimer = microtime(true);                                                                   
+        //$frame = str_replace('{'.$replace.'}', $this->apploader($app, $ini, $vars), $frame);       
+        $frame = str_replace('{'.$replace.'}', $this->apploader($controller, $ini, $vars), $frame); /
+/ needs to be preg replace so it is replaced 1 time                                                  
+        /*$this->app_timer['Frontpage ('.$count++.') - '.$app.'/'.$vars[0]] = microtime(true) - $ftim
+er; //timer for app*/                                                                                
+}                                                                                                    
+chdir($cwd);                                                                                         
+                                                                                                     
+echo $Parsedown->text($frame);                                                                       
